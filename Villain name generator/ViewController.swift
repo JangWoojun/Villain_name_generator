95c873285d31cf377be8f67131abb62822bf0e54
JangWoojun <woojun0107@naver.com>
2023-01-24 20:47:54 +0900

    design: 디자인 수정
    예상하지 않은 디자인 설계 실수 및 디자인 디테일 추가

--- Villain name generator/ViewController.swift
+++ Villain name generator/ViewController.swift
@@ -34,17 +34,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
         addressTextField.layer.borderColor = UIColor.systemGray2.cgColor
         addressTextField.layer.borderWidth = 5
         addressTextField.layer.cornerRadius = 10
+        addressTextField.attributedPlaceholder = NSAttributedString(string: "거주지(선택)", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        addressTextField.addLeftPadding()
         
         nameTextField.layer.borderColor = UIColor.systemGray2.cgColor
         nameTextField.layer.borderWidth = 5
         nameTextField.layer.cornerRadius = 10
-        
+        nameTextField.attributedPlaceholder = NSAttributedString(string: "이름", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        nameTextField.addLeftPadding()
+
         exNameLabel.clipsToBounds = true
         exAddressLabel.clipsToBounds = true
         exVillainNameLabel.clipsToBounds = true
         exAddressLabel.layer.cornerRadius = 10
         exNameLabel.layer.cornerRadius = 10
         exVillainNameLabel.layer.cornerRadius = 10
+        
     }
+    
+    @IBAction func createButtonTapped(_ sender: UIButton) {
+        if !lengthSwitch.isOn {
+            
 
+            
+        } else {
+            
+            
+            
+        }
+    }
+    
+}
+
+extension UITextField {
+  func addLeftPadding() {
+    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+    self.leftView = paddingView
+    self.leftViewMode = ViewMode.always
+  }
 }
