f650e6ac8f0da37e2ae9a04d8471719ab895c8da
JangWoojun <woojun0107@naver.com>
2023-01-24 10:45:22 +0900

    Initial Commit

---
+++ Villain name generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Villain name generator
+//
+//  Created by Jangwoojun on 2023/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
